6497fa5e1ebfaf26ca55c619bac78c86f8dd3586
chin <a5566qq2581@gmail.com>
2022-03-09 21:36:54 +0800

    fix: can not access with HTTP
    - remove auto redirect to HTTPS

--- FHIRValidatorAPI/Startup.cs
+++ FHIRValidatorAPI/Startup.cs
@@ -39,8 +39,6 @@ namespace FHIRValidatorAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
